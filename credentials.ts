cab17a676da08fe875296822ddf74b9ba7809e04
RuebenHales <halesrueben@gmail.com>
2022-02-24 17:00:08 -0500

    car stuff

---
+++ credentials.ts
@@ -0,0 +1,13 @@
+export defaults {
+
+  "type": "service_account",
+  "project_id": "ts-express-rh",
+  "private_key_id": "12295567260ad098feb3d8adaa559062bd1ce7a4",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCNvIX/lZ+dYICu\nZ10JBkxviYY2HgOsfFGSsXaHb1XWYlHd12SW68Z3Ar6rh3wv9zX1ALf8HvVJcyB/\ngU++M5JBMLX0exnhfvd2Kw2EUHTaw1J8TyQyeq8OVDOoldL+2WT8R2uuAI7u3S4s\nHyAwlR3TcE1WcHdPE5vwYr/rosKcF8fT1KRgQj4Qg3FsiK2Z+i3Ly5J7397XuJ/8\nBf1GnvixeE4rt846DqMSlVzw9TptAhD7xxmKPq+Cg1LJMbr5KZcAxwqx/gxQHku6\nEazq53uowCc8l5sHH+DwDxiUTUHq6CmPGAqNE32viSfQGgffIeiNE6KrfVItxv2N\neruirigjAgMBAAECggEAQG8nlbEOHowMA/0+iMdBwWUKTBjp/6SGqwMdSOW+5pbM\n8+ajNo8aKulnB/CwbtfaQjbg7W+fgBQj/oMpXuTwBM3hsjKyadaAJkrdOr0e2oL4\nQUXzRTWL1p5F28q84Jmw9JV9aN2dHQ8VGf7OcfegVwqTRgwb2SmndylVaYiVI0Np\nJLslz4J6IOWIycybVvr1G1mE6qSgkwm2uNW+pTw+kCIUH0mjBEM/94jxFr1Yebjs\neMEwmsSY4T/LG31q0spOqO4lagFyrvLMpc+WNLiLGOeVf3EDufOJ/7bqjQQxgDXM\np4cQUDIqfg7YJS60yrwQtwEuCT8cBWX+bcezPWsT2QKBgQC/qX58jBLhA8MF8OnR\nLwS0sy6yxDfdSB7rLF0lKDIu6gamfLw9r98is5YDKl132+7u0azpt99gazycupAM\ngbwVGCxjSKA4OKhIv6kuRmRpUgIyFiYGa0HPIH+cwhgwBOOhGZFy7j2tBjxADmL0\nW2MQH+uP86F9feTqYB/vw9JmawKBgQC9UKkpsSsOtX93vLjs0wus3PnBnL1t+Id/\nIRYh0i0eopkjucbixDDqz/ASe36/8Oy5MWMnxEUzDhOUHDHQN0rZoahebzKcacxt\n/gK+D6HdRarOSlwxQAHdw5Mf5j0S235fCmAsIFZA+vOrsNUK74AtNkpIoeC+T+ng\n3I75WXmDKQJ/cQyIju4QNKkDX326bKhPHkaSRN94FA12L0D7r+8FqqZfRdRNNBWI\nh+9ljHhwqUIQrM0AMNtD5ptaJRfl33qmdcrMY8NyvbatjDuAjsr2byjwIprt0ttw\n9bs1ikDyA2De4ycxo5jiyCvlXeMeewNsaxZo23/YwSyA2b3ZGJNohwKBgBLc8J4I\n2yNyDiteEIb4WFd3BSO3fGByvP17zHq+OGSvCtc2cYyyw7UB1wfgajaxyMSDGUAj\nHEoEAwtk68m1WK3qn54ZMk/HcukawCcz44rqoCRB4IG/wO/ZxtUM2dUBLcxGemKr\nLiz3VEQtMG0QhBL9L9Wg24Xa7JDNKSnLMoJRAoGALh0YzkzEomxI3s7Vt0F7TzOz\nJ44Qfe13/YXlVa9BV9lFbCRvOfkcjXwpWEGjmtrMSmXnu8MLqExTrS/nXPDEwFS4\ng4cyCVze8raOK7HZi5B6TJUj33d/fTiF+RaET/HHvsJFt8lYv6o9QqJD1MsfmX2u\n1dzksxEtbcAuB2WSeNY=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-bx1vg@ts-express-rh.iam.gserviceaccount.com",
+  "client_id": "104508514774007147471",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-bx1vg%40ts-express-rh.iam.gserviceaccount.com"
+}
---
+++ src/models/car.ts
@@ -0,0 +1,13 @@
+
+
+export interface Car {
+    make: string;
+    model: string;
+    year: number;
+    color?: string;
+    price?: number;
+    isElectric?: boolean;
+    used?: boolean;
+    vin?: string;
+    id?: string
+}
